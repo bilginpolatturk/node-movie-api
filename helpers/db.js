867340375b01a74c7eea1ff31183069257fbd371
bilginpolatturk <polatturkbilgin@gmail.com>
2021-10-14 15:17:01 +0300

    database connection

---
+++ helpers/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+
+    mongoose.connect('mongodb+srv://movie_user:123456123456@movie-api.7pk71.mongodb.net/movie-api?retryWrites=true&w=majority');
+    mongoose.connection.on('open', () =>{
+        console.log('MongoDB : Connected');
+    });
+
+    mongoose.connection.on('error', (err) =>{
+        console.log('MongoDB : Error',err);
+    });
+};
