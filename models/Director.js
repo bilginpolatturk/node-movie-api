ec170fb884bcc3eb0531cb36d863bbe4068e1f32
bilginpolatturk <polatturkbilgin@gmail.com>
2021-10-19 15:01:18 +0300

    movie and director validation

--- models/Director.js
+++ models/Director.js
@@ -4,9 +4,24 @@ const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
 
- name : String,
- surname : String,
- bio : String,
+ name :{
+    type : String,
+    required : true,
+    minlength : 2,
+    maxlength : 30
+ },
+ surname : {
+     type : String,
+     required : true,
+     minlength : 2,
+     maxlength : 30
+ },
+ bio : {
+     type : String,
+     required : true,
+     minlength : 2,
+     maxlength : 100
+ },
  createdAt : {
      type: Date,
      default : Date.now
--- models/Movie.js
+++ models/Movie.js
@@ -6,12 +6,35 @@ const MovieSchema = new Schema({
 
     title :{
         type : String,
-        required : true,
+        required : [true, '`{PATH}` alanı zorunludur.'],
+        maxlength : [20, '`{PATH}` alanı max (`{VALUE}`), ({MAXLENGTH}) karakter olmalıdır.'],
+        minlength : [2, '`{PATH}` alanı min (`{VALUE}`), ({MINLENGTH}) karakter olmalıdır.'],
     },
-    category : String,
-    country : String,
-    year : Number,
-    imdb : Number,
+    category : {
+        type  : String,
+        required : true,
+        minlength : 2,
+        maxlength : 20
+    },
+    country :{
+        type  : String,
+        required : true,
+        minlength : 2,
+        maxlength : 30
+    },
+    year :{
+        type : Number,
+        required : true,
+        max : 2040,
+        min : 1880
+    },
+    imdb : {
+        type : Number,
+        required : true,
+        min : 0,
+        max : 10
+
+    },
     director_id : Schema.Types.ObjectId,
     createdAt : {
         type : Date,
