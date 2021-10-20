31a0c2a7692171a1d1d81cc0c868e66f2e8cf6e7
bilginpolatturk <polatturkbilgin@gmail.com>
2021-10-20 16:02:36 +0300

    register endpoint and bcrypt password hash

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+// bcrypt password hash
+const bcrypt = require('bcryptjs');
+
+//User Model
+const User = require('../models/Users');
+
+
+/* DEAFULT */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
+
+// Register User Start -----
+router.post('/register', (req, res, next) => { 
+  const {username , password} = req.body;
+  //bcrypt password hash
+    bcrypt.hash(password, 10).then((hash) => {
+      const user =  new User({
+        username : username,
+        password : hash
+      });
+    
+      const promise = user.save();
+      promise.then((data) => {
+        res.json(data);
+      }).catch((err) => {
+        res.json(err);
+      });
+    });
+
+
+});
+// Register User End -----
+
 
 module.exports = router;
